e150c762eb3be2878b49f386c301fecb93a2bc83
tolyamask <maskalikanatoly@gmail.com>
2023-03-06 20:37:41 +0300

    remove unused lock

--- Freuders.Domain/Restaurant/Table/Lookup/Query.cs
+++ Freuders.Domain/Restaurant/Table/Lookup/Query.cs
@@ -4,18 +4,14 @@ namespace Freuders.Domain.Restaurant.Table.Lookup;
 
 public class Query : QueryExecutable<Request, Response>
 {
-    private readonly object _locker = new();
     private readonly Tables _tables;
 
     public Query(Tables tables) => _tables = tables;
 
     protected override Response Execute(Request request)
     {
-        lock (_locker)
-        {
-            var table = _tables.FirstOrDefault(table => table.Clients.Contains(request.Clients));
+        var table = _tables.FirstOrDefault(table => table.Clients.Contains(request.Clients));
 
-            return new Response(table);
-        }
+        return new Response(table);
     }
 }
